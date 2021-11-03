f747b374d50d19c580ff82ff4dbc7285eebd66ec
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-11-03 00:19:36 -0600

    Update Measure-LastCommand.ps1

--- Measure-LastCommand.ps1
+++ Measure-LastCommand.ps1
@@ -7,19 +7,41 @@ function Measure-LastCommand {
 
         [Parameter(Mandatory = $false)]
         [switch]
-        $ShowCommandLine
+        $ShowCommandLine,
+
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $Total
     )
     begin {}
     process {
         $LastCommands = Get-History -Count $History
         if ($LastCommands) {
+            $RunTimes = [System.Collections.Generic.List[psobject]]::new()
             foreach ($LastCommand in $LastCommands) {
                 if ($ShowCommandLine) {
                     $LastCommand.CommandLine
                 }
-                $RunTime = New-TimeSpan -End $LastCommand.EndExecutionTime -Start $LastCommand.StartExecutionTime
-                $Runtime
+                $RunTime = New-TimeSpan -Start $LastCommand.StartExecutionTime -End $LastCommand.EndExecutionTime
+                $RunTimes.Add($RunTime)
+                if ($Total) {
+                    continue
+                }
+                else {
+                    $RunTimes
+                }
             }
+            if ($Total) {
+                $TotalMilliseconds = 0
+                foreach ($RunTime in $RunTimes) {
+                    $TotalMilliseconds = $TotalMilliseconds + $RunTime.TotalMilliseconds
+                }
+                $RunTime = [TimeSpan]::FromMilliseconds($TotalMilliseconds)
+                $RunTime
+            }
+        }
+        else {
+            Write-Error -Message "No commands to measure."
         }
     }
     end {}
